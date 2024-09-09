8abad35adf75faf229e7df7442a8af91a9df6633
Daniel Muelle <danimr1999@gmail.com>
2024-09-09 17:01:50 +0200

    docs: account operations context

--- src/features/app/accounts/contexts/AccountOperations/useAccountOperationsContext.js
+++ src/features/app/accounts/contexts/AccountOperations/useAccountOperationsContext.js
@@ -1,7 +1,40 @@
+/** @import {Account} from "../../../../../docs" */
+
 import { useContext } from "react";
 
 import { AccountOperationsContext } from "./AccountOperationsContext";
 
+/**
+ * Custom hook to access account operations context.
+ *
+ * @function
+ * @returns {{
+ * selectAccount: function(Account): void,
+ * showAccountDetails: {
+ *  navigateToAccountDetails: function(): void
+ * },
+ * createAccount: {
+ *  isCreateAccountDialogOpened: boolean,
+ *  openCreateAccountDialog: function(): void,
+ *  closeCreateAccountDialog: function(): void
+ * },
+ * createTransaction: {
+ *  isCreateTransactionDialogOpened: boolean,
+ *  openCreateTransactionDialog: function(): void,
+ *  closeCreateTransactionDialog: function(): void
+ * },
+ * renameAccount: {
+ *  isRenameAccountDialogOpened: boolean,
+ *  openRenameAccountDialog: function(): void,
+ *  closeRenameAccountDialog: function(): void
+ * },
+ * deleteAccount: {
+ *  isDeleteAccountConfirmationDialogOpened: boolean,
+ *  openDeleteAccountConfirmationDialog: function(): void,
+ *  closeDeleteAccountConfirmationDialog: function(): void
+ * },
+ * }} Account operations context.
+ */
 export const useAccountOperationsContext = () => {
   const context = useContext(AccountOperationsContext);
 
